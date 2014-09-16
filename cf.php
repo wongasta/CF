9751ef3bffebad09b9d0af48cf547b2829ddb188
Yixin Xia <yixin_xia@volusion.com>
2014-09-16 16:18:07 -0500

    Major update
    Now displays multiple servers

--- cf.php
+++ cf.php
@@ -1,6 +1,6 @@
 <?php
 
-$ch = curl_init("https://entgaming.net/forum/games_fast.php");
+$ch = curl_init("https://entgaming.net/forum/games_all_fast.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 curl_close($ch);
@@ -8,25 +8,28 @@ curl_close($ch);
 $cfFind = '[ENT] Castle Fight!';
 $cfName = '';
 
+$cfArray = [];
+
 $arr = explode("\n", $data);
 
 foreach($arr as $val){
     if (strpos($val, $cfFind)!==false){
         $pos = strpos($val, $cfFind);
-        $cfNumber = substr($val, 0, 5);
+        $endLine = stripos($val,'|' );
+        $cfNumber = substr($val, 0, $endLine);
         $cfName = substr($val, $pos);
+
+        $ch2 = curl_init("https://entgaming.net/forum/slots_fast.php?id=" . $cfNumber);
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+        $metadata = curl_exec($ch2);
+        curl_close($ch2);
+        $gameOutput = array('name'=>$cfName, 'id'=>$cfNumber, 'meta'=>$metadata);
+
+        array_push($cfArray, $gameOutput);
     }
 }
-
-$ch = curl_init("https://entgaming.net/forum/slots_fast.php?id=" . $cfNumber);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$metadata = curl_exec($ch);
-curl_close($ch);
-
-
-$output = array('name'=>$cfName, 'id'=>$cfNumber, 'meta'=>$metadata);
 
 
 
 header('Content-Type: application/json');
-print $_GET['callback'] . '(' . json_encode($output) . ')';
+print $_GET['callback'] . '(' . json_encode($cfArray) . ')';
--- js/script.js
+++ js/script.js
@@ -12,12 +12,21 @@ var main =  {
                 console.log(error);
             },
             success: function(json) {
+                $.each(json, function(i,v){
+                    var gameAlias = 'game'+i;
+                    $('#gamesCont').append('<div class="game'+i+'"></div>');
+                    console.log(v);
+                    $('.'+gameAlias).html('<div class="gameName">'+ v['name']+'</div>');
+                    $('.'+gameAlias).append('<div class="gameMeta">'+ v['meta']+'</div>');
+                });
+
                 $('#gameInput').val(json.name);
                 $('.gameMeta').html(json.meta);
             }
         });
     },
     buttonUpdate: $('#refresh').click(function(){
+        $('#gamesCont').html('');
         main.update();
     })
 }
